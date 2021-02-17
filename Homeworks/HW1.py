1bcbce048922aa981f19ae3679bb1b20ee66f837
simgenurozbakir <simgenurozbakir@gmail.com>
2021-02-17 13:21:55 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,6 +1,14 @@
-n=3
-nonprimes = [j for i in range(2, 8) for j in range(i*2, 10, i)]
-primes = [x for x in range(2, 7) if x not in nonprimes]
+def prime_generator(end):
+    for n in range(2, end):     # n starts from 2 to end
+        for x in range(2, n):   # check if x can be divided by n
+            if n % x == 0:      # if true then n is not prime
+                break
+        else:                   # if x is found after exhausting all values of x
+            yield n             # generate the prime
 
-a = [primes for i in range(n)]
+
+g = prime_generator(7)       # give first 7 prime numbers
+x1 = list(g)
+
+a=[x1 for in range(3)]
 print(a)
